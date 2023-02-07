be75df368ef06a4144351f1b037bd7fcb359624d
clive <98222803+AAClive@users.noreply.github.com>
2023-02-07 07:20:35 +0000

    commit

--- deskenv/wasm-rs/src/main.rs
+++ deskenv/wasm-rs/src/main.rs
@@ -1,13 +1,19 @@
 #![allow(unused)]
 use wasm_bindgen::prelude::*;
-use std::mem;
+struct RsOnly;
+impl RsOnly{
+    fn browser_req() -> Result<(),Box<dyn Error>>{
+        let page=reqwest::blocking::get("https://httpbin.org/ip")?.text?;
+        page
+    }
+}
+
 #[wasm_bindgen]
-pub fn browser() -> String{
-    let page=reqwest::blocking::get("https://httpbin.org/ip")?.text()?;
-    let page = String::from_utf8(page.into()).unwrap();
-    page;
+fn browser_req()-> String{
+    resultrequest=RsOnly::browser_req.to_string();
+    resultrequest
 }
 
 fn main(){
-
+    
 }
