d8384e82096ff86b75557a10e2b06e10203c14e9
clive <98222803+AAClive@users.noreply.github.com>
2023-02-07 07:47:55 +0000

    commit

--- deskenv/wasm-rs/src/main.rs
+++ deskenv/wasm-rs/src/main.rs
@@ -13,15 +13,16 @@ use std::comm::SharedChan;
 impl RsOnly{
     fn browser_req(String url){
         if url.starts_with("https"){
-            let port = "80"
+            let port = "80";
         } else if url.starts_with("http"){
-            let port = "443"
-            let url=url.remove(0) //total:7
+            let port = "443";
+            for(x=0;x<7;x++){
+                let url=url.remove(0);
+            }
         }
         let addr: SocketAddr = from_str("{}:{}",url,port).unwrap();
         let listener=TcpListener::bind(addr).unwrap();
         let mut accepter=listener.listen().unwrap();
-        let (port,chan) 
     } //use sockets on port
 }
 
