fce3e0776ff2ce83024e3d8206f5a813374a0c6b
clive <98222803+AAClive@users.noreply.github.com>
2023-02-07 07:43:36 +0000

    commit

--- deskenv/wasm-rs/src/main.rs
+++ deskenv/wasm-rs/src/main.rs
@@ -1,16 +1,33 @@
 #![allow(unused)]
 use wasm_bindgen::prelude::*;
+use std::error::Error;
+use std::net;
 struct RsOnly;
+use std::str::StrSlice;
+use std::io::{TcpListener, TcpStream};
+use std::io::net::ip::SocketAddr;
+use std::io::{Listener, Writer, Acceptor, Buffer};
+use std::task::spawn;
+use std::io::BufferedStream;
+use std::comm::SharedChan;
 impl RsOnly{
-    fn browser_req() -> Result<(),Box<dyn Error>>{
-        let page=reqwest::blocking::get("https://httpbin.org/ip")?.text?;
-        page
-    }
+    fn browser_req(String url){
+        if url.starts_with("https"){
+            let port = "80"
+        } else if url.starts_with("http"){
+            let port = "443"
+            let url=url.remove(0) //total:7
+        }
+        let addr: SocketAddr = from_str("{}:{}",url,port).unwrap();
+        let listener=TcpListener::bind(addr).unwrap();
+        let mut accepter=listener.listen().unwrap();
+        let (port,chan) 
+    } //use sockets on port
 }
 
 #[wasm_bindgen]
-fn browser_req()-> String{
-    resultrequest=RsOnly::browser_req.to_string();
+pub fn browser_req()-> String{
+    let resultrequest=RsOnly::browser_req(url).to_string();
     resultrequest
 }
 
